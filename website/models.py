157ccf17cb01f713c7778d13d9501fc3346a2bbb
Karolline <hj264@hanmail.net>
2020-10-11 07:56:04 +0900

    메인 템플릿 생성

--- website/models.py
+++ website/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+class Candidate(models.Model):
+
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female')
+    )
+    
+    def user_path(instance, filename):
+        from random import choice
+        import string
+        
+        arr = [choice(string.ascii_letters) for _ in range(8)]
+        pid = ''.join(arr)
+        extension = filename.split('.')[-1]
+        
+        return 'website/photo/%s.%s' % (pid, extension)
+    
+    # user = models.ForeignKey
+    title = models.CharField(max_length=30)
+    # content
+    
+    # kind = models.CharField(
+    gender = models.CharField(max_length=2, choices=GENDER_CHOICES)
+    name = models.CharField(max_length=10)
+    
+    image = models.ImageField(upload_to = user_path)
+    # thumname_image = 
+    
+    reg_date = models.DateTimeField(auto_now_add = True)
+    
+    def register(self):
+        self.save()
+    
+    def __str__(self):
+        return self.name
+    
--- website/views.py
+++ website/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Candidate
 
+def list(request):
+    candidates = Candidate.objects.all()
+    str = ""
+    
+    for candidate in candidates:
+        str += "{}<br>{}".format(candidate.title, candidate.name)
+        
+    str += '<br>'
+    return HttpResponse(str)
+    
 def index(request):
-    return HttpResponse("Hello world")
+    return render(request, 'website/index.html', {})
 
 # Create your views here.
