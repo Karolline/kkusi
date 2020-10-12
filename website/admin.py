6d1175011ea3c3e15e0b14b06cb9b603fa99e75d
Karolline <hj264@hanmail.net>
2020-10-12 10:03:41 +0900

    모델 구조 수정

--- website/admin.py
+++ website/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Candidate
+from .models import Candidate, Post
 
 admin.site.register(Candidate)
+admin.site.register(Post)
--- website/models.py
+++ website/models.py
@@ -1,13 +1,6 @@
 from django.db import models
 
-class Candidate(models.Model):
-
-    GENDER_CHOICES = (
-        ('M', '남아'),
-        ('F', '여아')
-    )
-    
-    def user_path(instance, filename):
+def image_path(instance, filename):
         from random import choice
         import string
         
@@ -16,13 +9,22 @@ class Candidate(models.Model):
         extension = filename.split('.')[-1]
         
         return 'images/%s.%s' % (pid, extension)
+
+class Candidate(models.Model):
+
+    GENDER_CHOICES = (
+        ('M', '남아'),
+        ('F', '여아')
+    )
     
-    # kind = models.CharField(
-    gender = models.CharField(max_length=2, choices=GENDER_CHOICES)
     name = models.CharField(max_length=10)
-    # adopted
+    gender = models.CharField(max_length=2, choices=GENDER_CHOICES)
+    # kind = models.CharField(
+    birth_year = models.IntegerField()
     
-    image = models.ImageField(upload_to=user_path)
+    adopted = models.BooleanField()
+    
+    image = models.ImageField(upload_to=image_path)
     
     reg_date = models.DateTimeField(auto_now_add = True)
     
@@ -30,15 +32,25 @@ class Candidate(models.Model):
         self.save()
     
     def __str__(self):
-        return self.title
+        return self.name
         
-class User(models.Model):
-    
+    class Meta:
+        verbose_name_plural = "토끼 목록"
+        
+# class User(models.Model):
     
 class Post(models.Model):
+    # user = models.ForeignKey(User, on_delete=models.SET_DEFAULT, default='알 수 없는 사용자')
     title = models.CharField(max_length=30)
-    user = models.ForeignKey(User, on_delete=models.SET_DEFAULT, default='알 수 없는 사용자')
+    content = models.TextField()
     
-    cand = models.ForeignKey(Candidate, on_delete=models.SET_NULL)
+    cand = models.ForeignKey(Candidate, on_delete=models.SET_NULL, null=True)
+    
+    hide_yn = models.BooleanField()
     
     pub_date = models.DateTimeField(auto_now_add = True)
+    mod_date = models.DateTimeField()
+    
+class Photo(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.SET_NULL, null=True)
+    image = models.ImageField(upload_to=image_path)
