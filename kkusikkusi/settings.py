51ad97099df3db7d20e3f047bf52e235873b79bc
Karolline <hj264@hanmail.net>
2020-10-23 09:34:25 +0900

    collectstatic 1

--- kkusikkusi/settings.py
+++ kkusikkusi/settings.py
@@ -128,5 +128,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+ROOT_DIR = os.path.dirname(BASE_DIR)
+STATIC_ROOT = ROOT_DIR / ".static_root"
+
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / "media"
